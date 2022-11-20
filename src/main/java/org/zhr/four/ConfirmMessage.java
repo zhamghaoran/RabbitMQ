554abaf4ca5feb2dc93dc6f93397f540664b0922
zhanghaoran <2017929959@qq.com>
2022-11-20 16:23:37 +0800

    ""

--- src/main/java/org/zhr/four/ConfirmMessage.java
+++ src/main/java/org/zhr/four/ConfirmMessage.java
@@ -1,9 +1,13 @@
 package org.zhr.four;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 import org.zhr.utils.RabbitMqUtils;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * 发布确认
@@ -19,8 +23,10 @@ public class ConfirmMessage {
         // 单个确认
 //        ConfirmMessage.publishMessageIndividually();  //发布1000个单独确认消息，耗时99349ms
         // 批量确认
-        ConfirmMessage.publishMessageBatch();           //发布1000个批量确认消息，耗时28884ms
+        //ConfirmMessage.publishMessageBatch();           //发布1000个批量确认消息，耗时28884ms
         // 异步批量确认
+        ConfirmMessage.publishMessageAsync();  //发布1000个异步确认消息，耗时1633ms
+
     }
 
 
@@ -74,6 +80,61 @@ public class ConfirmMessage {
         long end = System.currentTimeMillis();
         System.out.println("发布" + MESSAGE_COUNT + "个" + "批量确认消息，耗时" + (end - begin) + "ms");
 
+    }
+
+    // 异步发布确认
+    public static void publishMessageAsync() throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        // 队列的声明
+        String queueName = UUID.randomUUID().toString();
+        channel.queueDeclare(queueName, true, false, false, null);
+        // 开启发布模式
+        channel.confirmSelect();
+        /**
+         * 线程安全有序的一个hash表 适用于高并发的情况下
+         * 1.能轻松的将序号和消息进行关联
+         * 2.轻松的批量删除 只要给到序号
+         * 3.支持高并发(多线程)
+         */
+        ConcurrentSkipListMap<Long,String> outstandingConfirms = new ConcurrentSkipListMap<>();
+        // 准备消息的监听器，那些消息成功了，那些消息失败了
+        // 消息确认成功回调函数
+        ConfirmCallback ackCallback = (deliveryTag, multiple) -> {
+            if (multiple) {
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(deliveryTag);
+                confirmed.clear();
+            } else {
+                outstandingConfirms.remove(deliveryTag);
+            }
+            // 2.删除掉已经确认的消息,剩下的就是未确认消息
+            System.out.println("确认的消息的编号" + deliveryTag);
+        };
+        // 消息确认失败回调函数
+        /**
+         * 1.第一个是消息的标记
+         * 2.第二个是是否批量确认
+         */
+        ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
+            String message = outstandingConfirms.get(deliveryTag);
+            // 打印为确认的消息
+            System.out.println("未确认的消息是" + message + "未确认的消息的标记 " + deliveryTag);
+        };
+        /**
+         * 1.监听那条消息成功了
+         * 2.监听那些消息失败了
+         */
+        channel.addConfirmListener(ackCallback, nackCallback);  // 异步通知
+        //开始时间
+        long begin = System.currentTimeMillis();
+        // 批量发送
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            String message = new String(String.valueOf(i));
+            channel.basicPublish("", queueName, null, message.getBytes());
+            //1.此处记录下所有要发送的消息
+            outstandingConfirms.put(channel.getNextPublishSeqNo(),message);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("发布" + MESSAGE_COUNT + "个" + "异步确认消息，耗时" + (end - begin) + "ms");
     }
 
 }
